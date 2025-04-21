fec6f5627c8305f1ba3783eae9ae21008c211fb9
vgkumar12-sys <58596030+vgkumar12-sys@users.noreply.github.com>
2025-04-21 22:52:26 +0530

    Mobile Header Padding Fixes

--- src/themes/client/layouts/NavigationBar.vue
+++ src/themes/client/layouts/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
 	<nav class="bg-gradient-to-r from-blue-600 to-blue-500 text-white py-2 top-0 z-10 w-full">
-		<div class="flex items-center px-6">
+		<div class="flex items-center px-2">
 			<slot name="flex-items"></slot>
 		</div>
 	</nav>
